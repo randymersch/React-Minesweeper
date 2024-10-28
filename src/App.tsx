7f8f323f70f6c4f1a0788b033e29d73b04fe7573
Randy Mersch <randymersch@hotmail.com>
2024-10-28 16:56:32 -0600

    initial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl text-red-600">Welcome to minesweeper</h1>
+      <div></div>
+    </>
+  );
+}
+
+export default App;
